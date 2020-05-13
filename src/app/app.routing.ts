9b58cd64ab9b1014c18fc146bd8b759f3c41afa7
Nam <namnv537@gmail.com>
2020-05-13 11:42:14 +0700

    fix route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -35,9 +35,7 @@ const routes: Routes =[
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
-       useHash: true
-    })
+    RouterModule.forRoot(routes)
   ],
   exports: [
   ],
